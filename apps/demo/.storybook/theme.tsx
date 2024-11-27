1e89341f01cb8779fdfdfe8b3822223b554fe732
Jason Santos <jason.santos@rangle.io>
2024-11-27 17:16:43 -0500

    fix: adds a dropdown for storybook

--- apps/demo/.storybook/theme.tsx
+++ apps/demo/.storybook/theme.tsx
@@ -1,20 +1,25 @@
-import React, { useEffect } from 'react';
-import type { Decorator } from '@storybook/react';
+import React, { useEffect } from "react";
+import type { Decorator } from "@storybook/react";
 
-const DEFAULT_BODY_CLASSES = ['sb-main-padded sb-show-main'];
+const DEFAULT_BODY_CLASSES = ["sb-main-padded sb-show-main"];
+
+import "@repo/foundations/styles";
 
 export const ThemeDecorator: Decorator = (Story, context) => {
-    const brand = context.globals.brands;
-    const mode = context.globals.modes;
-    const density = context.globals.density;
-    useEffect(() => {
-        document.body.classList.value = [
-            DEFAULT_BODY_CLASSES,
-            brand,
-            mode,
-            density,
-            document.body.style.backgroundColor = 'var(--semantic-static-color-background-default-default)'
-        ].join(' ');
-    }, [brand, mode, density]);
-    return <Story />;
+  const brand = context.globals.brands;
+  const mode = context.globals.modes;
+  const density = context.globals.density;
+  const breakpoint = context.globals.breakpoints;
+  useEffect(() => {
+    document.body.classList.value = [
+      DEFAULT_BODY_CLASSES,
+      brand,
+      mode,
+      density,
+      breakpoint,
+      (document.body.style.backgroundColor =
+        "var(--semantic-static-color-background-default-default)"),
+    ].join(" ");
+  }, [brand, mode, density, breakpoint]);
+  return <Story />;
 };
