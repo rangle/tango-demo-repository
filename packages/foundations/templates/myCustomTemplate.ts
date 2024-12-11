6352c5b8893cf297148e85dee888a1968bd5b154
Stephanie Zeng <stephanie.zeng@rangle.io>
2024-12-11 13:29:13 -0500

    feat: convert vector to svg

--- packages/foundations/templates/myCustomTemplate.ts
+++ packages/foundations/templates/myCustomTemplate.ts
@@ -3,10 +3,11 @@ import type { TemplateModule } from "radius-toolkit";
 export default {
   name: "my-custom-template",
   render: ({ layers, vectors }, options) => {
+    console.log("data", JSON.stringify(layers, null, 2));
     const transformed = vectors?.map(({ name, properties, source }) => {
       return {
         name,
-        properties: JSON.stringify(properties),
+        properties,
         source,
       };
     });
